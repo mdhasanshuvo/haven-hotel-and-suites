247fd333189a784ca359d2b8764c665f9cef02b2
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2024-12-26 20:04:09 +0600

    Book now button functionality added on special offer section

--- src/components/SpecialOffers .jsx
+++ src/components/SpecialOffers .jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const SpecialOffers = () => {
     const offers = [
         {
@@ -35,9 +37,9 @@ const SpecialOffers = () => {
                             <div className="p-6">
                                 <h3 className="text-2xl font-semibold text-gray-800 mb-4">{offer.title}</h3>
                                 <p className="text-lg text-gray-600 mb-4">{offer.description}</p>
-                                <button className="px-6 py-2 bg-primary text-white rounded-full hover:bg-accent-dark transition-colors">
+                                <Link to='/rooms' className="px-6 py-2 bg-primary text-white rounded-full hover:bg-accent-dark transition-colors">
                                     Book Now
-                                </button>
+                                </Link>
                             </div>
                         </div>
                     ))}

fb42c097fb08ffef4059cd2ee85a4fbca79eeb10
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-12 15:55:40 +0600

    My bookings private route arranged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
         <>
             <li><NavLink to='/'>Home</NavLink></li>
             <li><NavLink to='/rooms'>Rooms</NavLink></li>
-            <li><NavLink to='/gallery'>Gallery</NavLink></li>
-            <li><NavLink to='/about'>About Us</NavLink></li>
-            <li><NavLink to='/contact'>Contact</NavLink></li>
             {
                 user && <>
                     <li><NavLink to='/my-bookings'>My Bookings</NavLink></li>
                 </>
             }
+            <li><NavLink to='/gallery'>Gallery</NavLink></li>
+            <li><NavLink to='/about'>About Us</NavLink></li>
+            <li><NavLink to='/contact'>Contact</NavLink></li>
         </>
     );
 
